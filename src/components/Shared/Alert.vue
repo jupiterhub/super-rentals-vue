2c4c109537db9598b20693f2c204e7158cff917c
jupiterhub <jupiter.adverts@gmail.com>
2017-09-19 00:07:07 +0800

    fixed emit function, nissing $

--- src/components/Shared/Alert.vue
+++ src/components/Shared/Alert.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-alert error dismissable @input="onClose">
-
+  <v-alert error dismissible @input="onClose" :value="true">
+    This aler is closable
   </v-alert>
 </template>
 
@@ -8,7 +8,7 @@
 export default {
   methods: {
     onClose () {
-      this.emit('dismissed') // emit am event called dismissed (to be handled on parent)
+      this.$emit('dismissed') // emit am event called dismissed (to be handled on parent)
     }
   }
 }
