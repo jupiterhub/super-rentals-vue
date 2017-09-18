9bd2eae9c03be41245d72a371e913bc19dfa2578
jupiterhub <jupiter.adverts@gmail.com>
2017-09-18 23:37:59 +0800

    added implementation for signing in (super similar to signing up)

--- src/components/User/Signin.vue
+++ src/components/User/Signin.vue
@@ -1,5 +1,75 @@
 <template>
-  <div>
-    <p>The Signin Page</p>
-  </div>
+  <v-container>
+    <v-layout>
+      <!-- Display full width on smaller devices, for bigger ones only occupy 9/12 spaces -->
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card-text>
+          <v-container>
+            <form @submit.prevent="onSignin">
+              <v-layout row>
+                <!-- take the full width, but already constrained from the parent-->
+                <v-flex xs12>
+                  <v-text-field
+                    name="email"
+                    label="Email"
+                    id="email"
+                    v-model="email"
+                    type="email"
+                    required>
+                    </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="password"
+                    label="Password"
+                    id="password"
+                    v-model="password"
+                    type="password"
+                    required>
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-btn type="submit" class="info">Sign in</v-btn>
+                </v-flex>
+              </v-layout>
+            </form>
+          </v-container>
+        </v-card-text>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    user () {
+      return this.$store.getters.user // refer to the user that was created
+    }
+  },
+  watch: {
+    user (value) {  // watch the user() property from computed
+      if (value !== null && value !== undefined) {
+        // there is a user now, sign up success. redirect
+        this.$router.push('/')
+      }
+    }
+  },
+  methods: {
+    onSignin () {
+      // vuex
+      this.$store.dispatch('signUserIn', {email: this.email, password: this.password})
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -55,6 +55,23 @@ export const store = new Vuex.Store({
             console.log(error)
           }
         )
+    },
+    signUserIn ({commit}, payload) {
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          user => {
+            const newUser = {
+              id: user.uid,
+              requestedUnits: [] // TODO: implement when done
+            }
+            commit('setUser', newUser)
+          }
+        )
+        .catch(
+          error => {
+            console.log(error)
+          }
+        )
     }
   },
   getters: {
