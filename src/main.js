698d728d971851027da3e9f0dfeba67b0722dfe9
jupiterhub <jupiter.adverts@gmail.com>
2017-09-18 22:32:49 +0800

    imported firebase and intialize

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import './stylus/main.styl'
 import App from './App'
+import * as firebase from 'firebase' // import all exports from 'firebase' in a single file. Alternatively you can use named { initializeApp } for example
 import router from './router' // webpack, default looks at index.js file
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -19,5 +20,15 @@ new Vue({
   router,
   template: '<App/>',
   store,
-  components: { App }
+  components: { App },
+  created () {  // built-in hook called when Vue is finished initializing
+    firebase.initializeApp({
+      // taken from the firebase console
+      apiKey: 'AIzaSyC2x5EuG2LgdolBWlloT-sYJyf_T2TGHnk',
+      authDomain: 'superrentals-1505459904619.firebaseapp.com',
+      databaseURL: 'https://superrentals-1505459904619.firebaseio.com',
+      projectId: 'superrentals-1505459904619',
+      storageBucket: ''
+    })
+  }
 })
