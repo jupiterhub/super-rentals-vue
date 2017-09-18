4349be3c539d1fdb3bd5ca736e4740a9e3b071a7
jupiterhub <jupiter.adverts@gmail.com>
2017-09-18 16:06:22 +0800

    added page to display meetup details

---
+++ src/components/Rental/Rental.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-card-title>
+            <h6 class="primary--text">Grand Old Mansion</h6>
+          </v-card-title>
+          <v-card-media src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_%285%29.jpg"
+           height="400px">
+          </v-card-media>
+          <v-card-text>
+            <div>
+              This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="primary">Rent</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
