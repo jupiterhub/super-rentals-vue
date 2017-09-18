4ebe9277d0a496595ff2e3522743641625615f45
jupiterhub <jupiter.adverts@gmail.com>
2017-09-18 17:41:06 +0800

    dynamically load list based on data from vuex

--- src/components/Rental/Rentals.vue
+++ src/components/Rental/Rentals.vue
@@ -1,25 +1,25 @@
 <template>
   <v-container>
-    <v-layout row wrap>
+    <v-layout row wrap v-for="unit in units" class="mb-2">
       <v-flex xs12>
         <v-card class="info white--text">
           <!-- make another container so image only takes 5 columns -->
           <v-container>
             <v-layout>
             <v-flex xs5 sm4 md3>
-              <v-card-media src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_%285%29.jpg"
+              <v-card-media :src="unit.link"
                height="200px">
               </v-card-media>
             </v-flex>
             <v-flex xs7 sm8 md9>
               <v-card-title primary-title>
                 <div>
-                  <h5 class="mb-0">Property Name</h5>
+                  <h5 class="mb-0">{{unit.title}}</h5>
                   <div>Property Description</div>
                 </div>
               </v-card-title>
               <v-card-actions>
-                <v-btn flat dark to="/rentals/grand-old-mansion">
+                <v-btn flat dark :to="'/rentals/' + unit.id">
                   <v-icon left dark>arrow_forward</v-icon>
                    Check details
                 </v-btn>
@@ -32,3 +32,13 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+  export default {
+    computed: {
+      units () {
+        return this.$store.state.loadedUnits
+      }
+    }
+  }
+</script>
