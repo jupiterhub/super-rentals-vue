a25af5cc4479011ed7c3762cd4e66a469fd4fd62
jupiterhub <jupiter.adverts@gmail.com>
2017-09-18 20:51:14 +0800

    added page for adding a property

--- src/components/Rental/CreateRental.vue
+++ src/components/Rental/CreateRental.vue
@@ -1,5 +1,93 @@
 <template>
-  <div>
-    <p>Create Rental Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4 class="primary--text">Add a property</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>
+        <form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="unitName"
+                label="Property Name"
+                id="unit-name"
+                required
+                v-model="unitName">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="city"
+                label="City"
+                id="city"
+                required
+                v-model="city">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="imageUrl"
+                label="Image URL"
+                id="image-url"
+                required
+                v-model="imageUrl">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" height="200">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="description"
+                label="Description"
+                id="description"
+                multi-line
+                required
+                v-model="description">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary" :disabled="!formIsValid">Add Property</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      city: '',
+      unitName: '',
+      description: '',
+      imageUrl: ''
+    }
+  },
+  computed: {
+    formIsValid () {
+      return this.city !== '' &&
+        this.unitName !== '' &&
+          this.description !== '' &&
+          this.imageUrl !== ''
+    }
+  }
+}
+</script>
